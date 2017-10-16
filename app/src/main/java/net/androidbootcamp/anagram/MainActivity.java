c11cc9986c4688ce9baf738075f6db95dd8d60ae
JevinD <32504861+JevinD@users.noreply.github.com>
2017-10-16 01:25:21 -0700

    General Format
    Working on formatting the buttons

--- app/src/main/java/net/androidbootcamp/anagram/MainActivity.java
+++ app/src/main/java/net/androidbootcamp/anagram/MainActivity.java
@@ -1,33 +1,66 @@
 package net.androidbootcamp.anagram;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.app.Activity;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends AppCompatActivity {
+    private Button newGame;
+    private Button cont;
+    private Button selectStage;
+    private Button about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button newGame = (Button)findViewById(R.id.newGame);
-
-        newGame.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent int1=new Intent(MainActivity.this, NewGame.class);
-                startActivity(int1);
-            }
-        });
     }
+
+    public void onClickListenerButton() {
+        newGame = (Button) findViewById(R.id.newGame);
+        cont = (Button) findViewById(R.id.cont);
+        selectStage = (Button) findViewById(R.id.selectStage);
+        about = (Button) findViewById(R.id.about);
+    }
+       newGame.setOnClickListener(
+               new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            displayStr += "New Game";
+            display.setText(displayStr);
+        }
+    });
+        cont.setOnClickListener(
+                new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            displayStr += "Continue";
+            display.setText(displayStr);
+        }
+    });
+        selectStage.setOnClickListener(
+                new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            displayStr += "Select Stage";
+            display.setText(displayStr);
+        }
+    });
+        about.setOnClickListener(
+                new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            displayStr += "About";
+            display.setText(displayStr);
+        }
+    });
 
 }
